f5c1d1e08553cb1d65ce6588378dee186617ba72
Sam Willington <last_eyes@protonmail.com>
2023-12-10 13:44:39 +0900

    vercel deployment

---
+++ api/static/js/button_click.js
@@ -0,0 +1,6 @@
+document.querySelector('.gradientButton1').addEventListener('click', function() {
+    document.getElementById('formContainer').scrollIntoView();
+});
+document.querySelector('.gradientButton2').addEventListener('click', function() {
+    window.location.href = 'tel:031-506-1500';
+});
