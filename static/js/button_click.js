07341536be1ff2598c2a78327801cf47a0f30af8
Sam Willington <last_eyes@protonmail.com>
2023-12-10 13:54:36 +0900

    ah

--- static/js/button_click.js
+++
@@ -1,6 +0,0 @@
-document.querySelector('.gradientButton1').addEventListener('click', function() {
-    document.getElementById('formContainer').scrollIntoView();
-});
-document.querySelector('.gradientButton2').addEventListener('click', function() {
-    window.location.href = 'tel:031-506-1500';
-});
